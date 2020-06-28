b6af27b4b8e817ceb773e73a6c0401858aab0d9f
xiang-leiel <xianglei_family@sina.com>
2020-06-28 15:50:59 +0800

    feat 添加上报页面数据

--- project_approve/src/main/java/com/quantchi/tianji/service/search/model/WorkCircle.java
+++
@@ -1,7 +0,0 @@
-package com.quantchi.tianji.service.search.model;
-
-/**
- * 工作圈声明接口
- */
-public interface WorkCircle {
-}
--- project_approve/src/main/java/com/quantchi/tianji/service/search/model/param/DownLoadParams.java
+++
@@ -1,9 +0,0 @@
-package com.quantchi.tianji.service.search.model.param;
-/**
- * @Description 
- * @author leiel
- * @Date 2020/4/7 10:06 AM
- */
-
-public class DownLoadParams {
-}
